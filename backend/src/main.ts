b3c6bebe19cd3f18785ac6458ce6286b1908408e
karim <karimhosni67@gmail.com>
2026-02-17 22:08:30 +0200

    fixing undefined for frontend_url variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,32 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config'; // 1. Import ConfigService
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 2. Get ConfigService instance
+  const configService = app.get(ConfigService);
+
   const config = new DocumentBuilder()
     .setTitle('mini-revolut API')
     .setDescription('The mini-revolut API description')
     .setVersion('1.0')
-    .addBearerAuth({
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      in: 'header',
-      name: 'Authorization',
-      description: 'Enter your Bearer token',
-    },
-    'bearer'
-  )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+        name: 'Authorization',
+        description: 'Enter your Bearer token',
+      },
+      'bearer',
+    )
     .addSecurityRequirements('bearer')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs/api', app, documentFactory);
+
+  // 3. Use configService.get() for CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://mini-revault-app-production.up.railway.app'],
+    origin: [
+      configService.get('FRONTEND_URL'), 'http://localhost:5173',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
