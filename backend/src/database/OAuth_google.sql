9dbeb4e88318a0cc6359dd51729204b9b74ec5df
karim <karimhosni67@gmail.com>
2026-02-18 22:39:36 +0200

    adding passport-google-oauth20 in package.json

--- backend/src/database/OAuth_google.sql
+++ backend/src/database/OAuth_google.sql
@@ -1,5 +1,5 @@
 CREATE TYPE provider AS ENUM ('local', 'google');
 ALTER TABLE users add column "provider" provider;
-UPDATE users SET provider = 'local' WHERE provider IS NULL;
+UPDATE users SET provider = 'local' WHERE "provider" IS NULL;
 ALTER TABLE users ALTER COLUMN "provider" SET NOT NULL;
 ALTER TABLE users ALTER COLUMN password_hash DROP NOT NULL;
